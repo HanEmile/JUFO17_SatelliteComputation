424418e850ac9ad9acbc160c4355a4656f987dce
HanEmile <emile.hansmaennel@gmail.com>
2017-02-14 19:26:08 +0100

    able to read data from file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
+#include <string>
 
 int main(){
-  std::cout << "Download Data" << std::endl;
-  command
-  system("ls -l");
+  // system("ls -l");
+  // system("python3 download_data.py");
+
+  std::string line;
+  std::ifstream file ("TLE/argos.txt");
+
+  if(file.is_open()){
+    while(getline(file, line)){
+      std::cout << line << '\n';
+    }
+    file.close();
+  }
+
+  else{
+    std::cout << "Unable to open file";
+  }
+
   return 0;
 }
